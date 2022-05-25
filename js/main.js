cff49584a1e5a636278530e30ab6b22720872cca
Miguel Ramos <return.infox@gmail.com>
2022-05-25 19:47:11 -0400

    container con funcionalidad en javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('Hola Mundo')
+const container = document.querySelector('.container');
+
+// crear un elemento html
+const parrafo = document.createElement('p');
+
+// texto parrafo
+const textParrafo = document.createTextNode('Mi Parrafo');
+
+// añandiendo el texto al parrafo
+parrafo.appendChild(textParrafo);
+
+// renderizarlo en el Dom
+container.append(parrafo);
+
